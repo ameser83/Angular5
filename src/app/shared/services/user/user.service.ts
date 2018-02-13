167edf67b6d1ffb0d32b1dbf9353896b831f4685
ameser <ameser@gmail.com>
2018-02-13 01:00:25 -0500

    service implementation

--- src/app/shared/services/user/user.service.ts
+++ src/app/shared/services/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Http, Response } from '@angular/http';
-import 'rxjs/add/operator/map'
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 
@@ -12,13 +12,15 @@ export class User {
 
 export class UserService {
   users: User[];
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  getUsers() {
-    this.http.get('gte/user').subscribe(data => {console.log(data); this.users = data as User[];}, error => console.error(error));
-    return this.users;
-    //return this.http.get('gte/user').pipe(map(data => data['_body']));
-    //return this.http.get('gte/user').map((res: Response)=> res.json());
+  getUsers(): Observable<any> {
+    // this.http.get('gte/user').subscribe(data => {console.log(data); this.users = data as User[];}, error => console.error(error));
+    // return this.users;
+    return this.http.get('gte/user')
+    .pipe(
+      map(data => data['_body'])     
+    );
   }
 
 }
